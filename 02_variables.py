a9c07cc2ad6ea3c16e9a3f0b18818e7cde680b5a
Nima Mahdiyar <nimamhdeiyar@gmail.com>
2026-09-16 20:46:42 +0330

    Create 02_variables.py
    feat: add variables and data types

---
+++ 02_variables.py
@@ -0,0 +1,16 @@
+# Variables and Data Types
+
+name = "Nima"
+age = 20
+height = 182.0
+is_learning = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Learning Python:", is_learning)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_learning))
